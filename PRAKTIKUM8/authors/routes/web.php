7d4d91cc362d06687ea6a82360c022ba903443c4
Adria-Tisnawati-Aprilia <adriatisnawatiaprilia@gmail.com>
2022-05-23 10:43:23 +0700

    praktikum8n12

---
+++ PRAKTIKUM8/authors/routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+use Laravel\Lumen\Routing\Router;
+
+$router->get('/', function () use ($router) {
+
+    return $router->app->version();
+});
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+
+    $router->get('authors', ['uses' => 'AuthorController@showAllAuthor']);
+    $router->get('authors/{id}', ['uses' => 'AuthorController@showOneauthors']);
+    $router->post('authors', ['uses' => 'AuthorController@create']);
+    $router->delete('authors/{id}', ['uses' => 'AuthorController@delete']);
+    $router->put('authors/{id}', ['uses' => 'AuthorController@update']);
+    
+});
